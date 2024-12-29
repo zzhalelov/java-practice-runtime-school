bdf316e9e024d886b8cb3dbd6e4414a5f8acd9c1
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-12-30 00:24:01 +0500

    topic 'functional interfaces & lambdas': task_2, Function<T, R>

---
+++ src/main/java/kz/practice/functional_style/funcInterface_lambda/part_2/task_2.java
@@ -0,0 +1,24 @@
+package kz.practice.functional_style.funcInterface_lambda.part_2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+// Напишите метод, который принимает список чисел и объект интерфейса Function<Integer, Integer>.
+// Метод должен возвращать новый список, где к каждому числу применена переданная функция.
+public class task_2 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        Function<Integer, Integer> square = s -> (int) Math.pow(s, 2);
+        List<Integer> newNumbers = applyFunction(numbers, square);
+        System.out.println(newNumbers);
+    }
+
+    public static List<Integer> applyFunction(List<Integer> numbers, Function<Integer, Integer> function) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer integer : numbers) {
+            result.add(function.apply(integer));
+        }
+        return result;
+    }
+}
